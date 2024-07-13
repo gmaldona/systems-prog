b2ee8c8dae246819ab596ebd7118989a347b9a1b
gmaldona <gmaldona@oswego.edu>
2024-07-13 14:26:47 -0400

    more tests

--- gmalloc/test_main.c
+++ gmalloc/test_main.c
@@ -63,26 +63,30 @@ test_bitmap(void) {
 
 }
 
+void double_free_test() {
+  void *p1 = mem_mngr_alloc(7);
+  mem_mngr_free(p1);
+  mem_mngr_free(p1);
+}
+
+void realloc_mem_test() {
+  void *p1 = mem_mngr_alloc(7);
+  mem_mngr_free(p1);
+  void *p2 = mem_mngr_alloc(3);
+  printf("%p == %p\n", p1, p2);
+}
+
 void
 test_allocator() {
   void *p1 = mem_mngr_alloc(7);
+  // TODO: fix double free
   void *p2 = mem_mngr_alloc(7);
   void *p3 = mem_mngr_alloc(7);
   void *p4 = mem_mngr_alloc(7);
   void *p5 = mem_mngr_alloc(7);
-
-//  mem_mngr_free(p2);
   void *p6 = mem_mngr_alloc(7);
-
-  // // printf("%p == %p\n", p1, p6);
-  // // printf("%p != %p\n", p3, p4);
-
-  // // printf("Passed in unaligned pointer to free: \n");
-  // // mem_mngr_free(p6 + 1);
-
   void *p7 = mem_mngr_alloc(7);
   void *p8 = mem_mngr_alloc(7);
-
   void *p9 = mem_mngr_alloc(7);
   void *p10 = mem_mngr_alloc(7);
   void *p11 = mem_mngr_alloc(7);
@@ -99,20 +103,20 @@ test_allocator() {
   printf("pointer 10\t%p\n", p10);
   printf("pointer 11\t%p\n", p11);
 
-  // printf("Expecting two batches\n");
-  // mem_mngr_print_snapshot();
+   printf("Expecting two batches\n");
+   mem_mngr_print_snapshot();
 }
 
 int
 main(int argc, char *argv[]) {
   // Your code
 
-  unsigned char f[] = {0xF7, 0xFF, 0x01};
-//  bitmap_print_bitmap(f, sizeof(f));
 //  test_bitmap();
 
   mem_mngr_init();
-  test_allocator();
+//  double_free_test();
+//  realloc_mem_test();
+//  test_allocator();
 
   mem_mngr_leave();
 
