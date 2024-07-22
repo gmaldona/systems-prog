b24668b1a2d144681f42652d48df63921f701fa8
Gregory Maldonado <gmaldona@oswego.edu>
2024-07-21 21:55:20 -0400

    oz_demo init

---
+++ hw2/oz_demo.c
@@ -0,0 +1,19 @@
+/*
+ * CS 551 Homework 2
+ *
+ * author: Gregory Maldonado
+ * email : gmaldonado@cs.binghamton.edu
+ * date  : 2024-07-20
+ * web   : https://cs.binghamton.edu/~gmaldonado/
+ *
+ * Graduate student @ Thomas J. Watson College of Engineering and Applied
+ * Sciences, Binghamton University.
+ */
+
+//==================================================================== 80 ====>>
+
+int main(void) {
+   return 0;
+}
+
+//==================================================================== 80 ====>>
